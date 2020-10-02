7abc00c04e04df4b12b4b861ecd7433876dd245e
straylumo <gorbacheva.polina7394@gmail.com>
2020-10-02 12:21:47 +0300

    updated version of joystick

--- rc_controller.js
+++ rc_controller.js
@@ -2,6 +2,8 @@ var canvas, ctx;
 
 canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
+document.addEventListener('mouseup', stopDrawing);
+document.addEventListener('mousedown', startDrawing);
 document.addEventListener('mousemove', Draw);
 document.getElementById("x_coords").innerText = 0;
 document.getElementById("y_coords").innerText = 0;
@@ -33,13 +35,35 @@ function joystick(width, height) {
 }
 
 var coord = { x: 0, y: 0 };
+var start_drawing = false;
 
 function getPosition(event) {
     coord.x = event.clientX - canvas.offsetLeft;
     coord.y = event.clientY - canvas.offsetTop;
 }
 
+function startDrawing(event) {
+  start_drawing = true;
+  getPosition(event);
+  if (Math.pow(radius, 2) >= Math.pow(coord.x - x_center, 2) + Math.pow(coord.y - y_center, 2)) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      background();
+      joystick(coord.x, coord.y);
+      Draw();
+  }
+}
+
+function stopDrawing() {
+    start_drawing = false;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background();
+    joystick(width / 2, height / 3);
+    document.getElementById("x_coords").innerText = 0;
+    document.getElementById("y_coords").innerText = 0;
+}
+
 function Draw(event) {
+    if (start_drawing){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         background();
         var x, y;
@@ -59,4 +83,5 @@ function Draw(event) {
         document.getElementById("x_coords").innerText = Math.round(x - x_center);
         document.getElementById("y_coords").innerText = Math.round(y - y_center);
         send(Math.round(x - x_center), Math.round(y - y_center));
+      }
 }
