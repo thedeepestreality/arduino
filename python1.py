2d9ce08304eef8bdec4dbfc44b383db2bdc71c31
SvetlanaSarvartdinova <71510191+SvetlanaSarvartdinova@users.noreply.github.com>
2020-10-06 13:04:29 +0300

    server update

--- python1.py
+++ python1.py
@@ -1,17 +1,31 @@
+import asyncio
+import json
+import websockets
 import socket
-
-
-def int_to_bytes(i: int, *, signed: bool = False) -> bytes:
-    length = ((i + ((i * signed) < 0)).bit_length() + 7 + signed) // 8
-    return i.to_bytes(length, byteorder='big', signed=signed)
-
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-x = int_to_bytes(-12, signed=True)
-y = int_to_bytes(-344, signed=True)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(x, (UDP_IP, UDP_PORT))
-sock.sendto(y, (UDP_IP, UDP_PORT))
-print('Message sent')
+
+def func(dct):
+    return (int(dct['x']), int(dct['y']))
+
+
+async def connect(websocket, path):
+    async for message in websocket:
+        print(message)
+        a = json.dumps(message)
+        data = json.loads(a, object_hook=func)
+        #data = json.loads('{"x":1,"y":2}', object_hook=func)
+        print(data[0])
+        # x = str(data[0]).encode()
+        # y = str(data[1]).encode()
+        # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # sock.sendto(x, (UDP_IP, UDP_PORT))
+        # sock.sendto(y, (UDP_IP, UDP_PORT))
+
+
+start_server = websockets.serve(connect, "192.168.43.188", 5679)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
